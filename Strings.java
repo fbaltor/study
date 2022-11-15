febc764bec8ecbcd6169d326ff1ca13cca49b575
Felipe Baltor <fbaltor@gmail.com>
2022-11-15 20:21:13 -0300

    wip

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    
+    public static void main(String[] args) {
+        String str1;
+        str1 = "abc";
+
+        System.out.println(str1);
+        reverse();
+    }
+
+    public static String reverse() {
+        return "";
+    }
+ }
