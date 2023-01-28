dc00da095fc68943f907506eebc81417f6747a11
Felipe Baltor <fbaltor@gmail.com>
2023-01-27 22:16:29 -0300

    feat: char ascii conversion

---
+++ ConvertToAscii.java
@@ -0,0 +1,23 @@
+public class ConvertToAscii {
+    public static void main(String[] args) {
+
+        int a = (int) 'a';
+        int A = (int) 'A';
+
+        int z = (int) 'z';
+        int Z = (int) 'Z';
+
+        System.out.println("a: " + a);
+        System.out.println("A: " + A);
+        System.out.println("z: " + z);
+        System.out.println("Z: " + Z);
+
+        int charInt;
+        charInt = 'A';
+        System.out.println(charInt);
+
+        int length = 'z' - 'a' + 1;
+        System.out.println(length);
+
+    }
+}
