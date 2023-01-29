c547fbe30f44e8e7199c0b8e75298d66366d1a67
Felipe Baltor <fbaltor@gmail.com>
2023-01-28 22:35:04 -0300

    feat: list comparison

---
+++ ListComparison.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ListComparison {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+
+        System.out.println(list1.equals(list2));
+    }
+}
